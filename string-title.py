33e41706475a13b3737940f34238b77c2537ef39
Varun <jisbarunpandit@gmail.com>
2024-10-15 00:00:14 +0530

    Create string-title.py

---
+++ string-title.py
@@ -0,0 +1,15 @@
+str="Once there was a Man"
+a=str.title()
+print(a)
+
+##########another example
+str1="CROW WAS VERY THIRSTY"
+b=str1.title()
+print(b)
+###It returns true if the string follows titlecased###
+
+for i in range(0, 2):
+    print("Enter the string: ")
+    h= input()
+    print("Original String is : ", h)
+    print("String is title(): ", h.title(), "\n")
